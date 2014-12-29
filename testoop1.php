d480049c608feb8259eab7a8afb05ff35a616fcc
billtran2 <ngoantt@gmail.com>
2014-12-29 15:14:23 +0700

    Create testoop1.php
    Created

---
+++ testoop1.php
@@ -0,0 +1,63 @@
+<?php
+
+//Test use PHP and MySQL
+
+class book
+{
+    private $title;
+    private $isbn;
+    private $copies;
+
+    public function  __construct($isbn)
+    {
+        $this->setIsbn($isbn);
+    }
+    public function getIsbn()
+    {
+        echo "<p>ISBN : $this->isbn</p>";
+    }
+    function setIsbn($isbn)
+    {
+        $this->isbn=$isbn;
+    }
+    public function getTitle() {
+        //$this->title = "Easy PHP Websites with the Zend Framework";
+        print "Title: ".$this->title."<br />";
+    }
+    public function setTitle($title)
+    {
+        $this->title=$title;
+        //print "Title: ".$this->title."<br />";
+
+    }
+    public function getNumberCopies() {
+        $this->copies = "5";
+        print "Number copies available: ".$this->copies."<br />";
+    }
+
+}
+echo "OOP for PHP <br />";
+echo "----------------------------------------- <br />";
+$book = new book("0615303889");
+$book->setTitle("Beginning PHP and MySQL");
+$book->getTitle();
+$book->getNumberCopies();
+$book->getIsbn();
+$book->setIsbn('123');
+
+
+/*
+require("test.php");
+//include("test.php");
+$p="Ben ngoai ham";
+function change_pos()
+{
+    global $p;
+    $p='<br/>Ben Trong Ham';
+
+}
+change_pos();
+echo ("$p");
+ * */
+
+?>
